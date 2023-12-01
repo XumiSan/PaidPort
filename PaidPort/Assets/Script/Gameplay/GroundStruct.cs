5955bfef36196032ceafc93e4d4a4569fc04ca4a
XumiSan <daffahidayatulah123@gmail.com>
2023-12-01 14:42:18 +0700

    AddSaveProsedural

---
+++ PaidPort/Assets/Script/Gameplay/GroundStruct.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundStruct : MonoBehaviour
+{
+
+}
+
+
+[System.Serializable]
+public class GroundState
+{
+    public List<int> data;
+}
--- PaidPort/Assets/Script/Gameplay/ProseduralMap.cs
+++ PaidPort/Assets/Script/Gameplay/ProseduralMap.cs
@@ -21,85 +21,133 @@ public class ProseduralMap : MonoBehaviour
     private int groundWidth = 10;
     [SerializeField]
     private int groundHeight = 10;
-    [SerializeField]
-    private float bronzeChance = 0.05f;
-    [SerializeField]
+    private float bronzeChance = 0.03f;
     private float silverChance = 0.03f;
-    [SerializeField]
     private float goldChance = 0.02f;
-    [SerializeField]
-    private float diamondChance = 0.01f;
+    private float diamondChance = 0.02f;
     [SerializeField]
     private int maxDepth = 50;
 
     private int currentDepth = 0;
 
+    public Transform groundContainer;
+
+    public bool isLoad;
+
+
+    public GroundState state;
+
+
+    public GroundState state1;
+
+    private void Awake()
+    {
+    }
+
     void Start()
     {
-        
+
         GenerateInitialGround();
     }
 
     void Update()
     {
-        
+
         if (Camera.main.transform.position.y > spawnPosition.y - (groundHeight * groundCollider.localScale.y) && currentDepth < maxDepth)
         {
             GenerateGround();
         }
+
     }
 
-  void GenerateInitialGround()
+    void GenerateInitialGround()
     {
-        for (int i = 0; i < groundWidth; i++)
+        int currentLayerDepth = currentDepth;
+        for (int j = 0; j < groundHeight; j++)
         {
-            for (int j = 0; j < groundHeight; j++)
+            for (int i = 0; i < groundWidth; i++)
             {
                 Vector3 spawnPos = new Vector3(spawnPosition.x + i * groundCollider.localScale.x, spawnPosition.y - j * groundCollider.localScale.y, 0);
                 float randomValue = Random.value;
-                int currentLayerDepth = -currentDepth;
 
                 if (currentLayerDepth >= -2)
                 {
-                    Instantiate(groundPrefab, spawnPos, Quaternion.identity);
+                    Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
                 }
             }
+
+            currentLayerDepth = -currentDepth;
         }
+
         currentDepth += groundHeight;
     }
 
     void GenerateGround()
     {
+
         for (int i = 0; i < groundWidth; i++)
         {
+            Transform currentGround = groundContainer;
+            int groundType = 0;
             Vector3 spawnPos = new Vector3(spawnPosition.x + i * groundCollider.localScale.x, spawnPosition.y - (groundHeight * groundCollider.localScale.y), 0);
             float randomValue = Random.value;
             int currentLayerDepth = -currentDepth;
 
             if (currentLayerDepth >= -2)
             {
-                Instantiate(groundPrefab, spawnPos, Quaternion.identity);
+                Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                currentGround = ground;
             }
             else if (currentLayerDepth >= -150)
             {
                 if (randomValue <= bronzeChance)
                 {
-                    Instantiate(bronzePrefab, spawnPos, Quaternion.identity);
+                    if (isLoad)
+                    {
+                        Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                        currentGround = ground;
+                    }
+
+                    if (!isLoad)
+                    {
+
+                        Transform ground = Instantiate(bronzePrefab, spawnPos, Quaternion.identity, groundContainer);
+                        groundType = 1;
+                        currentGround = ground;
+                    }
+
                 }
                 else
                 {
-                    Instantiate(groundPrefab, spawnPos, Quaternion.identity);
+                    Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                    currentGround = ground;
                 }
             }
             else if (currentLayerDepth >= -300)
             {
                 if (randomValue <= silverChance)
                 {
-                     Instantiate(silverPrefab, spawnPos, Quaternion.identity);
+
+
+
+                    if (isLoad)
+                    {
+                        Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                        currentGround = ground;
+                    }
+
+                    if (!isLoad)
+                    {
+
+                        Transform ground = Instantiate(silverPrefab, spawnPos, Quaternion.identity, groundContainer);
+                        groundType = 2;
+                        currentGround = ground;
+                    }
                 }
                 else
                 {
-                    Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity);
+                    Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                    currentGround = ground;
                     GroundHealth groundHealth = ground.GetComponent<GroundHealth>();
                     if (groundHealth != null)
                     {
@@ -111,11 +159,26 @@ public class ProseduralMap : MonoBehaviour
             {
                 if (randomValue <= goldChance)
                 {
-                  Instantiate(goldPrefab, spawnPos, Quaternion.identity);  
+                    if (isLoad)
+                    {
+                        Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                        currentGround = ground;
+                    }
+
+                    if (!isLoad)
+                    {
+
+                        Transform ground = Instantiate(goldPrefab, spawnPos, Quaternion.identity, groundContainer);
+                        groundType = 3;
+                        currentGround = ground;
+                    }
+
+
                 }
                 else
                 {
-                    Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity);
+                    Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                    currentGround = ground;
                     GroundHealth groundHealth = ground.GetComponent<GroundHealth>();
                     if (groundHealth != null)
                     {
@@ -127,12 +190,26 @@ public class ProseduralMap : MonoBehaviour
             {
                 if (randomValue <= diamondChance)
                 {
-                    Instantiate(diamondPrefab, spawnPos, Quaternion.identity);
+
+                    if (isLoad)
+                    {
+                        Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                        currentGround = ground;
+                    }
+
+                    if (!isLoad)
+                    {
+
+                        Transform ground = Instantiate(diamondPrefab, spawnPos, Quaternion.identity, groundContainer);
+                        groundType = 4;
+                        currentGround = ground;
+                    }
                 }
 
                 else
                 {
-                    Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity);
+                    Transform ground = Instantiate(groundPrefab, spawnPos, Quaternion.identity, groundContainer);
+                    currentGround = ground;
                     GroundHealth groundHealth = ground.GetComponent<GroundHealth>();
                     if (groundHealth != null)
                     {
@@ -140,8 +217,89 @@ public class ProseduralMap : MonoBehaviour
                     }
                 }
             }
+
+
+
+            if (groundType != 0)
+            {
+
+                int index = currentGround.GetSiblingIndex();
+                GameManager.Instance.groundListType.Add(groundType);
+                GameManager.Instance.groundListTypeIndex.Add(index);
+
+            }
+
+            //GameManager.Instance.groundState
         }
         spawnPosition.y -= groundCollider.localScale.y;
         currentDepth += groundHeight;
+
+        int childCount = groundContainer.childCount;
+        Debug.Log(childCount);
+        if (childCount == 8096)
+        {
+            if (isLoad)
+            {
+                LoadMap();
+
+            }
+
+            else
+            {
+                SaveMap();
+            }
+        }
+    }
+
+    private void SaveMap()
+    {
+        state.data = GameManager.Instance.groundListType;
+        state1.data = GameManager.Instance.groundListTypeIndex;
+        string json = JsonUtility.ToJson(state);
+        string json1 = JsonUtility.ToJson(state1);
+
+        Debug.Log(json);
+        Debug.Log(json1);
+
+        PlayerPrefs.SetString("mapsave", json);
+        PlayerPrefs.SetString("mapsave1", json1);
+    }
+    private void LoadMap()
+    {
+        string json = PlayerPrefs.GetString("mapsave");
+        string json1 = PlayerPrefs.GetString("mapsave1");
+        state = JsonUtility.FromJson<GroundState>(json);
+        state1 = JsonUtility.FromJson<GroundState>(json1);
+
+        int stateCount = state.data.Count;
+
+
+        for (int i = 0; i < stateCount; i++)
+        {
+            GameObject go = groundContainer.GetChild(state1.data[i]).gameObject;
+            int states = state.data[i];
+
+            Vector3 curPos = go.transform.position;
+            go.SetActive(false);
+
+            switch (states)
+            {
+                case 4:
+                    Instantiate(diamondPrefab, curPos, Quaternion.identity, groundContainer);
+                    break;
+                case 3:
+                    Instantiate(goldPrefab, curPos, Quaternion.identity, groundContainer);
+                    break;
+                case 2:
+                    Instantiate(silverPrefab, curPos, Quaternion.identity, groundContainer);
+                    break;
+                case 1:
+                    Instantiate(bronzePrefab, curPos, Quaternion.identity, groundContainer);
+                    break;
+                default:
+                    print("Incorrect intelligence level.");
+                    break;
+            }
+        }
     }
 }
